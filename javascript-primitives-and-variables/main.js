10b73433132df0753128d94bac5d31dc8460439d
Lisa Nguyen <lisanguyensc@gmail.com>
2022-08-29 16:57:47 +0000

    Completed javascript-primitives-and-variables - Lisa Nguyen

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,15 @@
+var fullName = 'Lisa Nguyen';
+var isCool = true;
+var totalPets = 2;
+var unknown = null;
+let never;
+console.log('value of FullName', fullName);
+console.log('value of isCool', isCool);
+console.log('value of totalPets', totalPets);
+console.log('typeof fullName', typeof fullName);
+console.log('typeof isCool', typeof isCool);
+console.log('typeof totalPets', typeof totalPets);
+console.log('value of', unknown);
+console.log('typeof unknown', typeof unknown);
+console.log('value of never', never);
+console.log('typeof never', typeof never);
