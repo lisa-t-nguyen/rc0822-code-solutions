8d1b75ef5064d8538973f948e00e2c35a2c9bccf
Lisa Nguyen <lisanguyensc@gmail.com>
2022-08-29 18:05:38 +0000

    Completed  JS Operators and Expressions - Lisa Nguyen

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 45;
+var height = 67;
+var area = width * height;
+console.log('value of area', area);
+console.log('typeof area', typeof area);
+
+var bill = 900;
+var payment = 3000;
+var change = payment - bill;
+console.log('value of change', change);
+console.log('typeof change', typeof change);
+
+var quizzes = 80;
+var midterm = 78;
+var final = 93;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade', grade);
+console.log('typeof grade', typeof grade);
+
+var firstName = 'Lisa';
+var lastName = 'Nguyen';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName', fullName);
+console.log('typeof fullName', typeof fullName);
+
+var pH = 90;
+var isAcidic = pH < 7;
+console.log('value of isAcidic', isAcidic);
+console.log('typeof isAcidic', typeof isAcidic);
+
+var headCount = 67;
+var isSparta = headCount === 300;
+console.log('value of isSparta', isSparta);
+console.log('typeof isSparta', typeof isSparta);
+
+var motto = fullName;
+motto = fullName + ' ' + 'is the GOAT';
+console.log('value of motto', motto);
+console.log('typeof motto', typeof motto);
