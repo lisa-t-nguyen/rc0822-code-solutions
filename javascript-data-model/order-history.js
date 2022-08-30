37e5b1a372a51b39ab9f7439d0c0ee329899e1da
Lisa Nguyen <lisanguyensc@gmail.com>
2022-08-30 16:39:49 +0000

    Completed javascript-data-model - Lisa Nguyen

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,49 @@
+var orderHistory = [
+  {
+    orderplaced: 'August 4th, 2020',
+    totalcost: '$34',
+    costofitem: '$31.55',
+    shippedto: 'JS Masher',
+    ordernumber: '114-3941689-8772232',
+    delivered: 'August 8th, 2020',
+    item: 'JavaScript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    returnwindow: 'Return window closed on September 7th, 2020'
+  },
+  {
+    orderplaced: 'July 19th, 2020',
+    totalcost: '$44.53',
+    costofitem: '$41.33',
+    shippedto: 'JS Masher',
+    ordernumber: '113-9984268-1280257',
+    delivered: 'July 20th, 2020',
+    deliverymethod: 'Your package was delivered. It was handed directly to a resident.',
+    item: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    returnwindow: 'Return window closed on August 19th, 2020'
+  },
+  {
+    orderplaced: 'July 4th, 2020',
+    totalcost: '$17.22',
+    costofitem: '$15.98',
+    shippedto: 'JS Masher',
+    ordernumber: '114-2875557-9059409',
+    delivered: 'July 7th, 2020',
+    deliverymethod: 'Your package was delivered. It was handed directly to a resident.',
+    item: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    returnwindow: 'Return window closed on August 5th, 2020'
+  },
+  {
+    orderplaced: 'July 3rd, 2020',
+    totalcost: '$138.93',
+    costofitem: '$94.95',
+    costofitem2: '$33.99',
+    shippedto: 'JS Masher',
+    ordernumber: '113-2883177-2648248',
+    delivered: 'July 5th, 2020',
+    item: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+    item2: 'The Art of Sql',
+    author: 'Faroult, Stephanie',
+    returnwindow: 'Return window closed on August 4th, 2020'
+  }
+];
