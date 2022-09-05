7bfb1f6a2bcd9466abf4ddd4ee300735a413c5a4
Lisa Nguyen <lisanguyensc@gmail.com>
2022-09-05 19:36:29 +0000

    Completed dom-querying - Lisa Nguyen

---
+++ dom-querying/main.js
@@ -0,0 +1,17 @@
+var one = document.querySelector('h1');
+console.log('h1 element', one);
+console.dir('console dir', one);
+
+var explanation = document.querySelector('#explanation');
+console.log('#explanation', explanation);
+console.dir('console dir explanation', explanation);
+
+var hint = document.querySelector('.hint');
+console.log('class hint', hint);
+console.dir('console dir hint', hint);
+
+var pelements = document.querySelectorAll('p');
+console.log('all p elements', pelements);
+
+var classelements = document.querySelectorAll('.example-link');
+console.log('all class elements', classelements);
