45561985f43b7490522f5dba0c50052944a232d2
Lisa Nguyen <lisanguyensc@gmail.com>
2022-09-09 00:03:39 +0000

    Completed javascript-typing-tutor, updated code in JS - Lisa Nguyen

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -3,8 +3,12 @@ var spanLocation = 0;
 
 document.addEventListener('keydown', function (event) {
   if (event.key === characters[spanLocation].textContent) {
-    characters[spanLocation].className = 'text-green';
+    characters[spanLocation].className = 'text-under';
+    if (event.key === characters[spanLocation].textContent) {
+      characters[spanLocation].className = 'text-green';
+    }
     spanLocation++;
+    characters[spanLocation].className = 'text-under';
   } else {
     characters[spanLocation].className = 'text-red';
   }
