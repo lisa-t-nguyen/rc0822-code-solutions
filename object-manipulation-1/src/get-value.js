da7c903b1268aba4c41800d03292e502bf7e916e
Lisa Nguyen <lisanguyensc@gmail.com>
2022-09-10 04:35:48 +0000

    Completed object-manipulation-1 - Lisa Nguyen

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,6 @@
+/* exported getValue */
+// define function getValue with two parameters, object and key
+// return object at key index to obtain the value
+function getValue(object, key) {
+  return object[key];
+}
