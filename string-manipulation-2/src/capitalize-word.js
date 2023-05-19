85430b7fc9f268cefe1936afc9f85c3de71235df
Lisa Nguyen <lisanguyensc@gmail.com>
2023-05-19 19:42:09 +0000

    Completed Capitalized Word algo

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -1,1 +1,10 @@
 /* exported capitalizeWord */
+function capitalizeWord(word) {
+  if (word.toLowerCase() === 'javascript') {
+    return 'JavaScript';
+  }
+  const firstLetter = word.slice(0, 1);
+  const afterFirstLetter = word.slice(1, word.length);
+
+  return firstLetter.toUpperCase().concat(afterFirstLetter.toLowerCase());
+}
