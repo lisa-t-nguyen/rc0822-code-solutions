5e5183c65a32b9a339b1a08cdd7b65fa09830c00
Lisa Nguyen <lisanguyensc@gmail.com>
2023-06-08 03:24:00 +0000

    Started on anagram algo

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,29 @@
 /* exported isAnagram */
+function isAnagram(firstString, secondString) {
+  firstString = firstString.replace(' ', '');
+  secondString = secondString.replace(' ', '');
+
+  if (firstString.length !== secondString.length) {
+    return false;
+  }
+
+  const firstMap = new Map();
+  const secondMap = new Map();
+
+  for (let i = 0; i < firstString.length; i++) {
+    const char = firstString.charAt(i);
+    firstMap.set(char, (firstMap.get(char) || 0) + 1);
+  }
+
+  for (let i = 0; i < secondString.length; i++) {
+    const char = secondString.charAt(i);
+    secondMap.set(char, (secondMap.get(char) || 0) + 1);
+  }
+
+  if (firstMap.size !== secondMap.size) {
+    return false;
+  }
+  // console.log('secondMapKeys', secondMap.keys());
+  // console.log('firstMapKeys', firstMap.keys());
+  return false;
+}
