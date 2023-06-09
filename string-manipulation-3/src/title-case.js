1b748e109a6d3617732d1e4fccca962882e6edf2
Lisa Nguyen <lisanguyensc@gmail.com>
2023-06-09 02:22:13 +0000

    Started title-case algo

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,9 @@
 /* exported titleCase */
+// RULES:
+// 1. JavaScript is always JavaScript
+// 2. API is always API
+// 3. and, or, nor, but, a, an, the, as, at, by, for, in, of, on, per, to are always lowercase except when:
+//    a. They're the first word of the title (beginning of string)
+//    b. They're the first word of the subtitle (beginning of after a colon)
+// 4. Capitalize all words of four letters or more.
+// 5. The second part of a hyphenated word should also be capitalized (Self-Report, not Self-report)
