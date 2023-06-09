fc59538acece82d72f5103370b7a8960e0bef9c5
Lisa Nguyen <lisanguyensc@gmail.com>
2023-06-09 01:36:51 +0000

    Completed reverse words algo

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,27 @@
 /* exported reverseWords */
+
+// Split each string at ' '
+// Reverse each string that have been split
+// Put them back together by concatenating each string in order
+// Return new string
+
+function reverseWords(string) {
+  const stringArray = string.split(' ');
+  let newerString = '';
+
+  for (let i = 0; i < stringArray.length; i++) {
+    newerString = newerString + reverseString(stringArray[i]) + ' ';
+  }
+
+  return newerString.trimEnd();
+}
+
+function reverseString(string) {
+  let newString = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString = newString + string[i];
+  }
+
+  return newString;
+}
